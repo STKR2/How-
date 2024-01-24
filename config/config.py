30d49afb6c371310de7e39e093909d873aced4fb
Wa <122730917+STKR2@users.noreply.github.com>
2024-01-24 18:36:40 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -57,7 +57,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamYukki/YukkiMusicBot",
+    "https://github.com/STKR2/Owner",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
