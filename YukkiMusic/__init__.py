19e23fcaef9bb035937bee1e33561ba1a7d6d411
Wa <122730917+STKR2@users.noreply.github.com>
2024-01-26 14:56:45 +0300

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -1,8 +1,8 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+from YukkiMusic.core.bot import Anony
+from YukkiMusic.core.dir import dirr
+from YukkiMusic.core.git import git
+from YukkiMusic.core.userbot import Userbot
+from YukkiMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
