b4ba859586e59e1dee088d17099833acdf42fdbd
Wa <122730917+STKR2@users.noreply.github.com>
2024-01-26 22:27:21 +0300

    Update resume.py

--- YukkiMusic/plugins/admins/resume.py
+++ YukkiMusic/plugins/admins/resume.py
@@ -12,6 +12,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
+from strings.filters import command
 from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
@@ -23,7 +24,7 @@ RESUME_COMMAND = get_command("RESUME_COMMAND")
 
 
 @app.on_message(
-    filters.command(RESUME_COMMAND)
+    command(RESUME_COMMAND)
     & filters.group
     & ~BANNED_USERS
 )
