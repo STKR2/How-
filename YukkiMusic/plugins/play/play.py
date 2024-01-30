c611b75d2d6613ec8e8cad5d765f60a847c76ba5
Wa <122730917+STKR2@users.noreply.github.com>
2024-01-30 06:07:36 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -41,7 +41,7 @@ PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 
 @app.on_message(
-   filters.command(["play", "vplay", "cplay", "cvplay", "playforce", "vplayforce", "cplayforce", "cvplayforce"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"])     
+   filters.command(["شغل", "فيديو", "تشغيل", "فيد", "playforce", "vplayforce", "cplayforce", "cvplayforce"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"])     
     & filters.group
     & ~BANNED_USERS
 )
